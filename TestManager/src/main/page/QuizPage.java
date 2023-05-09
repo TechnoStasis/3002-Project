577e2d7396bfcda3790498d7b0f199c8652835a9
Yves Reyes <yvesreyes@Yvess-MacBook-Air.local>
2023-05-09 17:12:23 +0800

    auto redirecting quiz page

--- TestManager/src/main/page/QuizPage.java
+++ TestManager/src/main/page/QuizPage.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.sun.net.httpserver.HttpExchange;
 
@@ -19,23 +21,36 @@ public class QuizPage extends AbstractPageHandler {
 
     @Override
     public void handleGet(HttpExchange t) throws IOException {
+
+        if (!t.getRequestURI().toASCIIString().contains("?=")) {
+            ArrayList<String> redir = new ArrayList<>();    
+            redir.add("quiz?=1");
+            t.getResponseHeaders().put("Location", redir);
+            t.sendResponseHeaders(302, -1);
+            t.close();
+        }
+
+        String currentQuestion = t.getRequestURI().toASCIIString().split("=")[1];
+
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("questionnumber", currentQuestion);
+
+        String htmlPage = HtmlRenderer.render(this.htmlPage, data);
+
         t.sendResponseHeaders(200, htmlPage.length());
         t.getResponseBody().write(htmlPage.getBytes());
         t.getResponseBody().close();
+
     }
 
     @Override
     public void handlePost(HttpExchange t) throws IOException {
-
 
         InputStream io = t.getRequestBody();
         InputStreamReader inputStreamReader = new InputStreamReader(io);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         String req = bufferedReader.readLine().replace("+", " ");
         String[] details = req.split("&");
-
-        System.out.println(req);
-
 
     }
 
