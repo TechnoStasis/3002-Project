0cda5f9ff569c8a5992f44d2be01c6632b5bf324
RatKing2002 <nikethkarlapudi75@gmail.com>
2023-05-12 23:57:50 +0800

    New client that can preserve form+ transfer data

--- ProtocolTest/Client_2.java
+++ ProtocolTest/Client_2.java
@@ -3,13 +3,15 @@ import java.io.*;
 
 public class Client_2 {
     public static void main(String[] args) throws IOException {
-        String serverHostname = "192.168.0.2";  //needs to know servers ip in order to be able to do anything
+        String serverHostname = "192.168.0.14";  //needs to know servers ip in order to be able to do anything
         int serverPort = 1234;
 
         Socket clientSocket = new Socket(serverHostname, serverPort);
         System.out.println("COnnected to server: " + clientSocket); //testing what server connected to
+        
 
-        InputStream inputStream = clientSocket.getInputStream();     
+        InputStream inputStream = clientSocket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));     
         byte[] syn = new byte[1]; //client sends Syn
         inputStream.read(syn);
  
@@ -21,21 +23,42 @@ public class Client_2 {
             outputStream.write(synAck);
             System.out.println("SYN-ACK sent");
 
+            //Create file writer
+            BufferedWriter fileWriter = new BufferedWriter(new FileWriter("serverdata.txt"));
+
             byte[] ack = new byte[1]; //client recieves ack
             inputStream.read(ack);
             if (ack[0] == 0x03){
                 System.out.println("ACK recieved");
+                //Loop to get all questions from merged questions file
+                String recievedData;
+                while ((recievedData = reader.readLine()) != null){
+                    System.out.println("Recieved data: " + recievedData);
 
-                String message = "Hello from the Client!, all acknlowedgments went through!";  //testing writing message to server
+                    fileWriter.write(recievedData);
+                    fileWriter.newLine();
+
+                    byte[] dataAck = {0x04};
+                    outputStream.write(dataAck);
+                    System.out.println("ACK sent for data");
+
+                }
+
+
+            /*     String message = "Hello from the Client!, all 3 acknlowedgments went through!";  //testing writing message to server
                 byte[] messageBytes = message.getBytes();
-                outputStream.write(messageBytes);
+                outputStream.write(messageBytes);   */
+             /*    byte[] dataAck = {0x04};
+                outputStream.write(dataAck);
+                System.out.println("ACK sent for data");  */
 
-                byte[] responseBytes = new byte[1024];  //testing recieveing data from server
-                int bytesRead = inputStream.read(responseBytes);
-                String response = new String(responseBytes,0, bytesRead);
-                System.out.println("Recieved message from server: " + response);
+            //    byte[] responseBytes = new byte[1024];  //testing recieveing data from server
+            //    int bytesRead = inputStream.read(responseBytes);
+            //    String response = new String(responseBytes,0, bytesRead);
+            //    System.out.println("Recieved message from server: " + response);   
 
-                clientSocket.close();
+                fileWriter.close();  //close file
+                clientSocket.close();  //close socket
 
             }
             else{
