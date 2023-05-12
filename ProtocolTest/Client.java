73fef88f925f7be4f4383fae2cd99c78fd8195af
RatKing2002 <nikethkarlapudi75@gmail.com>
2023-05-12 20:58:24 +0800

    Modified to test sending data while preserving

--- ProtocolTest/Client.java
+++ ProtocolTest/Client.java
@@ -6,44 +6,53 @@ public class Client {
         String serverHostname = "192.168.0.2";  //needs to know servers ip in order to be able to do anything
         int serverPort = 1234;
 
-        Socket clientSocket = new Socket(serverHostname, serverPort);
-        System.out.println("COnnected to server: " + clientSocket); //testing what server connected to
 
-        InputStream inputStream = clientSocket.getInputStream();     
-        byte[] syn = new byte[1]; //client sends Syn
-        inputStream.read(syn);
- 
-        if (syn[0] == 0x01){  //test if client got a syn
-            System.out.println("SYN recieved");
+        try(Socket clientSocket = new Socket(serverHostname, serverPort)){
 
-            OutputStream outputStream = clientSocket.getOutputStream(); //send syn-ack to server
-            byte[] synAck = {0x02};
-            outputStream.write(synAck);
+            InputStream input = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            OutputStream output = clientSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output,true);
+
+            //Create file writer
+            BufferedWriter fileWriter = new BufferedWriter(new FileWriter("serverdata.txt"));
+
+            //Send SYN-ACK to server
+            writer.println((char)0X02);
             System.out.println("SYN-ACK sent");
 
-            byte[] ack = new byte[1]; //client recieves ack
-            inputStream.read(ack);
-            if (ack[0] == 0x03){
+            //Recieve ACK from server
+            int ack = reader.read();
+            if (ack == 0x03) {
                 System.out.println("ACK recieved");
+                
 
-                String message = "Hello from the Client!, all acknlowedgments went through!";  //testing writing message to server
-                byte[] messageBytes = message.getBytes();
-                outputStream.write(messageBytes);
+                //Loop to receive questions
+                String recievedData;
+                while ((recievedData = reader.readLine()) != null){
+                    System.out.println("Recieved data: " + recievedData);
 
-                byte[] responseBytes = new byte[1024];  //testing recieveing data from server
-                int bytesRead = inputStream.read(responseBytes);
-                String response = new String(responseBytes,0, bytesRead);
-                System.out.println("Recieved message from server: " + response);
+                    fileWriter.write(recievedData);
+                    fileWriter.newLine();
 
-                clientSocket.close();
+                    // Send ACK for recieved data
+                    writer.println((char)0x04);
+                    System.out.println("ACH sent for data");
+                }
 
+                //Close file writer
+                fileWriter.close();
+
+
+            } else {
+                System.out.println("Failed to receive ACK");
             }
-            else{
-                System.out.println("Failed to recieve ACK");
-            }
-        }
-        else{
-            System.out.println("Failed to recieve SYN");
-        }
+        } catch (UnknownHostException ex){
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex){
+            System.out.println("I/O error: " + ex.getMessage());
+        }     
+        
     }
 }
